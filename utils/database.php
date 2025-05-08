cb99be3b86fda3b2b38e395f4b8d9e4b46a4d997
Sam.K <119893889+skakiswa77@users.noreply.github.com>
2025-05-08 21:15:32 +0200

    Database

---
+++ utils/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'BusinessCare';
+$username = 'root';
+$password = 'root';
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
